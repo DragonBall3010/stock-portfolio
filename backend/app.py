0862c2b26f14112c5bfe8875757fdd9901792299
lower_intelligence <74339072+DragonBall3010@users.noreply.github.com>
2024-12-02 01:43:05 +0530

    Add MySQL database connection

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# MySQL configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'sql123'
+app.config['MYSQL_DB'] = 'stock_portfolio'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/test-db')
+def test_db():
+    cur = mysql.connection.cursor()
+    cur.execute("SHOW DATABASES;")
+    databases = cur.fetchall()
+    return {"databases": [db[0] for db in databases]}
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
